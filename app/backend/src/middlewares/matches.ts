eb2cc6db83c2ab0e3cdf6d5f004025a1375c46db
Matheus Rodrigues <luz.matheus11@gmail.com>
2022-08-05 12:22:44 -0300

    feat: validation exist teams

--- app/backend/src/middlewares/matches.ts
+++ app/backend/src/middlewares/matches.ts
@@ -1,12 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
+import Teams from '../database/models/Teams';
 
 export default class MatchesValidate {
-  public validations = (req: Request, res: Response, next: NextFunction) => {
+  public validations = async (req: Request, res: Response, next: NextFunction) => {
     const { homeTeam, awayTeam } = req.body;
 
     if (homeTeam === awayTeam) {
       return res.status(401).json(
         { message: 'It is not possible to create a match with two equal teams' },
+      );
+    }
+
+    const homeTeamExist = await Teams.findOne({ where: { id: homeTeam } });
+    const awayTeamExist = await Teams.findOne({ where: { id: awayTeam } });
+
+    if (!homeTeamExist || !awayTeamExist) {
+      return res.status(404).json(
+        { message: 'There is no team with such id!' },
       );
     }
 
